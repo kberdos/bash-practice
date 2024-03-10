a8bdb1c4d3bbfa729ddd8fa7f82884bc30c8d549
Kazuya Erdos <kazuya_erdos@brown.edu>
2024-03-10 15:27:02 -0400

    first exercise

--- getinfo.sh
+++ getinfo.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 
-# echo the time and date 
-
 DATE=$(date)
+USERS=$(who)
+UPTIME=$(uptime)
+
 echo "Current time and date: $DATE"
+echo "Users"
+echo "$USERS"
+echo "Uptime: $UPTIME"
+
+# clear log.txt (dangerous?)
+> log.txt
+# write all of the variables to log.txt
+# -e flag interprets the escape characters
+echo -e "$DATE\n$USERS\n$UPTIME" >> log.txt
+
+# exit code same as the echo
 exit
